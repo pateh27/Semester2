22d74e9afc808cb2d1c2a0d9d8ffa786fefa31f6
pateh27 <fathzfl@gmail.com>
2024-02-21 22:52:34 +0700

    done

---
+++ Week2/Dragon19.java
@@ -0,0 +1,25 @@
+package Week2;
+
+public class Dragon19 {
+    int x, y, width, heigth;
+    
+    void moveLeft(){
+        x -= 1;
+    }
+    void moveRigth(){
+        x += 1;
+    }
+    void moveUp(){
+        y += 1;
+    }
+    void moveDown(){
+        y -= 1;
+    }
+    void detectCollision(int x, int y){
+        if ((x < 0 || x >= width)||(y < 0 || y >= heigth)) {
+            System.out.println("GAME OVER");
+        }
+    }
+
+    
+}
---
+++ Week2/Dragonmain19.java
@@ -0,0 +1,29 @@
+package Week2;
+
+public class Dragonmain19 {
+   public static void main(String[] args) {
+    Dragon19 dg1 = new Dragon19();
+    dg1.x = 3;
+    dg1.y = 5;
+    dg1.width = 10;
+    dg1.heigth = 10;
+
+    dg1.moveRigth();
+    dg1.detectCollision(dg1.x, dg1.y);    
+    dg1.moveRigth();
+    dg1.detectCollision(dg1.x, dg1.y);    
+    dg1.moveRigth();
+    dg1.detectCollision(dg1.x, dg1.y);    
+    dg1.moveLeft();
+    dg1.detectCollision(dg1.x, dg1.y);    
+    dg1.moveLeft();
+    dg1.detectCollision(dg1.x, dg1.y);    
+    for (int i = 1; i <= 10;) {
+        dg1.moveUp();
+        i++;   
+    }    
+    dg1.detectCollision(dg1.x, dg1.y);    
+    }
+   
+   
+}
